5ac9608fd0dad2533efe9c6b1eb9b98c17d22751
munazirdzuana <munazir805@gmail.com>
2023-01-01 01:43:15 +0700

    update

--- app/src/main/java/co/id/uinsgd/kel3foodrecipe/activities/About.kt
+++ app/src/main/java/co/id/uinsgd/kel3foodrecipe/activities/About.kt
@@ -2,11 +2,17 @@ package co.id.uinsgd.kel3foodrecipe.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import co.id.uinsgd.kel3foodrecipe.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        buttonb.setOnClickListener(){
+            finish()
+        }
     }
 }
